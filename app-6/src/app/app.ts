b88f2b0720eeba9b71f7d175d993cf5d95f1d6c0
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-19 19:52:54 +0530

    common built-in pipes

--- app-6/src/app/app.ts
+++ app-6/src/app/app.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('app-6');
+
+  name = signal("pranav kumar");
+  date = new Date();
+  amount = signal(1234);
+
+  data = {
+    name: "Pranav",
+    roll: 168,
+    email: "abc@xyz.com"
+  };
 }
