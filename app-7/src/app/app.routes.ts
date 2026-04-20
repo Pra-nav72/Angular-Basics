96e82f885bdbbb6b767d2f608ef061f58846682c
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-20 21:14:38 +0530

    routing basics

--- app-7/src/app/app.routes.ts
+++ app-7/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component:Home},
+    {path:"contact", component:Contact},
+    {path:"about", component:About},
+];
