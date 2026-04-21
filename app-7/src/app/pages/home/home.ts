0c466e917d87f5c57f8ce0ebf36f02df5982a784
Pranav Kumar <Gyannik7772@gmail.com>
2026-04-21 19:43:48 +0530

    navigate through pages onClick

--- app-7/src/app/pages/home/home.ts
+++ app-7/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  constructor(public router: Router){}
+  goToAbout(){
+    this.router.navigate(['about']);
+  }
+}
